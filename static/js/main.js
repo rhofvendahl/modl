2ac0734bc0fce2173d4ef9a9569b1675befc9453
Russell Hofvendahl <rhofvendahl@gmail.com>
2018-12-07 22:39:29 -0800

    lay groundwork for frontend

--- static/js/main.js
+++ static/js/main.js
@@ -1,34 +1,37 @@
 window.onload = function() {
-  parseTree = new ParseTree();
+  // model = new Model();
 
-  var input = document.getElementById('query');
-  parseTree.process(input.value);
-
+  var input = document.getElementById('page');
   input.focus();
   input.selectionStart = input.value.length;
   input.selectionEnd = input.value.length;
 
+  // model.update(input.value);
+
   // // don't update constantly if timeOuts overlap
   // var timeOuts = 0;
-  // document.oninput = function(event) {
-  //   var updated = false;
-  //   if (timeOuts == 0) {
-  //     parseTree.process(input.value);
-  //     var updated = true;
-  //   }
-  //   timeOuts += 1
-  //   setTimeout(function() {
-  //     timeOuts -= 1;
-  //     if (timeOuts == 0 && !updated) parseTree.process(input.value);
-  //   }, 100)
-  // };
+  document.oninput = function(event) {
+    // event.which?
+    console.log(event.code)
+    // if (event.code == )
+    // var updated = false;
+    // if (timeOuts == 0) {
+    //   parseTree.process(input.value);
+    //   var updated = true;
+    // }
+    // timeOuts += 1
+    // setTimeout(function() {
+    //   timeOuts -= 1;
+    //   if (timeOuts == 0 && !updated) parseTree.process(input.value);
+    // }, 100)
+  };
   //
   // // update at intervals if timeOuts overlap
   // setInterval(function() {
   //   if (timeOuts > 1) parseTree.process(input.value)
   // }, 100);
 
-  setInterval(function() {
-    parseTree.process(input.value)
-  }, 5000);
+  // setInterval(function() {
+  //   model.update(input.value);
+  // }, 3000);
 }
